00399ebe1080e970c0deb6a714b449fd900fc776
xjtrail-code <xjtrail@gmail.com>
2026-03-24 16:37:51 -0400

    exercise 2 is complete

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+# added comment 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
